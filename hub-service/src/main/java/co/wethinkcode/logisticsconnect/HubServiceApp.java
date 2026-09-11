3bb70f8d444bcd9e1b58fc2496ca31d2e0a2baf1
Ayanda Mtolo <aymtojhb025@student.wethinkcode.co.za>
2026-09-11 10:23:39 +0200

    Connect hub service to ingestion service

--- hub-service/src/main/java/co/wethinkcode/logisticsconnect/HubServiceApp.java
+++ hub-service/src/main/java/co/wethinkcode/logisticsconnect/HubServiceApp.java
@@ -1,6 +1,11 @@
 package co.wethinkcode.logisticsconnect;
 
 import io.javalin.Javalin;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.io.IOException;
 
 public class HubServiceApp {
 
@@ -8,8 +13,24 @@ public class HubServiceApp {
         Javalin app = Javalin.create().start(7051);
 
         app.get("/health", ctx -> ctx.result("OK"));
+        app.get("/hubs", ctx -> ctx.result(getHubsFromIngestion()));
 
         // TODO (Serves provinces and sorting centers (place-name source of truth).)
         // Add domain endpoints for hub-service here.
+    }
+
+    public static String getHubsFromIngestion() {
+        HttpClient client = HttpClient.newHttpClient();
+
+        HttpRequest request = HttpRequest.newBuilder().uri(URI.create("http://localhost:7050/hubs")).GET().build();
+
+        try {
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+            return response.body();
+
+        }catch (IOException | InterruptedException e) {
+            return "Error conecting to ingestion service: " + e.getMessage();
+        }
     }
 }
