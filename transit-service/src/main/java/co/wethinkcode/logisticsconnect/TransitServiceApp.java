c3672c8163e7f5381ad0ea3034368ef6ab86c1ff
Ayanda Mtolo <aymtojhb025@student.wethinkcode.co.za>
2026-09-12 09:01:24 +0200

    Connect transit service to delay stage service

--- transit-service/src/main/java/co/wethinkcode/logisticsconnect/TransitServiceApp.java
+++ transit-service/src/main/java/co/wethinkcode/logisticsconnect/TransitServiceApp.java
@@ -1,6 +1,8 @@
 package co.wethinkcode.logisticsconnect;
 
 import io.javalin.Javalin;
+import org.eclipse.jetty.util.IO;
+
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
@@ -24,10 +26,26 @@ public class TransitServiceApp {
         }
     }
 
+    public static String getDelayStageFromDelayService(String hubId) {
+        HttpClient client = HttpClient.newHttpClient();
+
+        HttpRequest request = HttpRequest.newBuilder().uri(URI.create("http://localhost:7052/delay-stage/" + hubId)).GET().build();
+
+        try {
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+            return response.body();
+
+        } catch (IOException | InterruptedException e) {
+            return "Error connecting to delay stage service " + e.getMessage();
+        }
+    }
+
     public static void main(String[] args) {
         Javalin app = Javalin.create().start(7053);
 
         app.get("/health", ctx -> ctx.result("OK"));
+
 
         // TODO (Calculates estimated arrival windows based on hub and delay stage.)
         // Add domain endpoints for transit-service here.
